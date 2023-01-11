4ef85c0d1108b27a2eb26704e73b1000df64b8a3
json <json@kakao.com>
2023-01-12 01:21:26 +0900

    Static prerender 사이트 설정으로 변경

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto'
+import adapter from '@sveltejs/adapter-static'
 import { vitePreprocess } from '@sveltejs/kit/vite'
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -6,12 +6,7 @@ const config = {
   // Consult https://kit.svelte.dev/docs/integrations#preprocessors
   // for more information about preprocessors
   preprocess: vitePreprocess(),
-
-  kit: {
-    adapter: adapter({
-      fallback: '200.html',
-    }),
-  },
+  kit: { adapter: adapter() },
 }
 
 export default config
